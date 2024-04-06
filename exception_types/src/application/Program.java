8959843e98b635bbaf8372a0e77db80f50c39c15
HugoGuilherme <hugo159357@gmail.com>
2024-04-06 11:52:29 -0300

    feature(Exceptions): including examples

---
+++ exception_types/src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import java.text.ParseException;
+
+import examples.VeryBadSolution;
+
+public class Program {
+
+	public static void main(String[] args) throws ParseException  {
+
+		VeryBadSolution.main();
+	}
+
+}
